1857ced85db1d3d93100cc726656bf60a665b7de
Tupis <joaoh.tupinamba@gmail.com>
2022-09-23 01:57:56 -0300

    feat: import and use router

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,7 @@
 import * as express from "express";
-import { Request, Response } from "express";
+import router from "./routes/routes";
 
 const app = express();
+app.use(router);
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World");
-});
-
-app.listen(3000, () => console.log("Server Ativo"));
+app.listen(3000, () => console.log("Server Online!"));
